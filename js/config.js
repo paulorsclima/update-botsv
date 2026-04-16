32a33c262a51b8a6635a5f0b6b89a296ff4b6311
paulorsclima <paulo.rsclima@gmail.com>
2026-04-16 14:24:17 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // Carregado PRIMEIRO pelos outros arquivos JS
 // ============================================================
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxvVUy78wqgglMPrVGrx_O5WbJgGy8ESggZAqvZzhQaGcVMQ5IE4Bxm5cThSsgmCvQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzJDEH7_fTdtlOte6D9Zj3bZtyHgxk0hM53WZABGHb7ARp71cj8TjlRQqanXUo7kFM/exec";
 
 // Dados globais (preenchidos pelo loadData via API)
 let DATA          = null;
